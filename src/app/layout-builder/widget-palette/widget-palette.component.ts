2a3160a07da8cdf1fa3c7eb9b7727e5fb084b700
rogue-BYTE <vedantanandva28@gmail.com>
2026-01-30 13:10:46 +0530

    feat: Widget palette

--- src/app/layout-builder/widget-palette/widget-palette.component.ts
+++ src/app/layout-builder/widget-palette/widget-palette.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Widget } from '../../models/widget.model';
 
 @Component({
   selector: 'app-widget-palette',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./widget-palette.component.css']
 })
 export class WidgetPaletteComponent {
+
+  availableWidgets: Partial<Widget>[] = [
+    { type: 'text', label: 'Text Input', width: 200, height: 60 },
+    { type: 'button', label: 'Button', width: 100, height: 40 },
+    { type: 'card', label: 'Card', width: 300, height: 200 },
+    { type: 'chart', label: 'Chart', width: 400, height: 300 }
+  ];
 
 }
